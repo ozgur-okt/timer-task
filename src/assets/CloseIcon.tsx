c2be9f7da7e11db0879bc597b99cb08cbbae1f9f
Özgür Oktay <oktayozgur94@gmail.com>
2024-04-20 15:21:38 +0300

    fix:prop indent

--- src/assets/CloseIcon.tsx
+++ src/assets/CloseIcon.tsx
@@ -6,9 +6,29 @@ type IconProps = {
 
 function CloseIcon({ color }: IconProps) {
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M15 9.00002L9 15M8.99997 9L14.9999 15" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-      <circle cx="12" cy="12" r="9" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15 9.00002L9 15M8.99997 9L14.9999 15"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <circle
+        cx="12"
+        cy="12"
+        r="9"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
     </svg>
   )
 }
